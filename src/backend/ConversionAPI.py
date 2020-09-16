34c70fff3b7648b23153798846836ee72c8eed43
Sam <LordPenguin6@yahoo.com>
2020-09-16 18:17:10 +0000

    Fixed some API errors

--- src/backend/ConversionAPI.py
+++ src/backend/ConversionAPI.py
@@ -5,17 +5,17 @@ app = Flask('app')
 cors = CORS(app)
 
 @app.route('/FtoC')
-  def FtoC(F):
-    C = (F - 32) × 5/9
-    data = {
-      "return": C
-    }
-    return json.jsonify(data)
+def FtoC(F):
+  C = (F - 32) * 5/9
+  data = {
+    "return": C
+  }
+  return json.jsonify(data)
 
 @app.route('/CtoF')
-  def CtoF(C)
-    F = (C × 9/5) + 32
-    data = {
-      "return": F
-    }
-    return json.jsonify(data)
+def CtoF(C):
+  F = (C * 9/5) + 32
+  data = {
+    "return": F
+  }
+  return json.jsonify(data)
