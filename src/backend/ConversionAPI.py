7486e709468dff731852918289e4df1e434e1979
Sam <LordPenguin6@yahoo.com>
2020-09-16 19:02:05 +0000

    Fixed more errors(Still no way to send input)

--- src/backend/ConversionAPI.py
+++ src/backend/ConversionAPI.py
@@ -4,18 +4,27 @@ from flask import json
 app = Flask('app')
 cors = CORS(app)
 
-@app.route('/FtoC')
-def FtoC(F):
-  C = (F - 32) * 5/9
+@app.route('/')
+def default():
   data = {
-    "return": C
+    "return": "No input was given"
   }
   return json.jsonify(data)
 
-@app.route('/CtoF')
-def CtoF(C):
-  F = (C * 9/5) + 32
-  data = {
-    "return": F
+@app.route('/FtoC')
+def FtoC():
+  C1 = (70 - 32) * 5/9
+  data1 = {
+    "return": C1
   }
-  return json.jsonify(data)
+  return json.jsonify(data1)
+
+@app.route('/CtoF')
+def CtoF():
+  F2 = (70 * 9/5) + 32
+  data2 = {
+    "return": F2
+  }
+  return json.jsonify(data2)
+
+app.run(host='0.0.0.0', port=8080)
