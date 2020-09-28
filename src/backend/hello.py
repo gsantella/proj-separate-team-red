b09446b80fa1d15fccf0cef155187ea52c6eb699
Guido Santella <gsantella@southhills.edu>
2020-09-28 15:57:51 -0400

    update devops

--- src/backend/hello.py
+++ src/backend/hello.py
@@ -1,16 +1,11 @@
-from flask import Flask
-from flask import json
-from flask import request
-app = Flask('app')
+from flask import Flask, json
+app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return json.jsonify({"return": "Hello"})
+@app.route('/api2/howdy')
+def howdy():
+    return json.jsonify({"Howdy": "World"})
 
-@app.route('/howdy')
-def world():
-    return json.jsonify({"return": "World"})
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+#@app.route('/', defaults={'path': ''})
+#@app.route('/<path:path>')
+#def catch_all(path):
+#    return json.jsonify({"Hello": "World"})
