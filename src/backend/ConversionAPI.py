1f39ce35e9f0d1955de926f9b11e15bbc57127de
TNTBlasterZapper <LordPenguin6@yahoo.com>
2020-10-12 15:26:52 -0400

    Improved Names and added comments

--- src/backend/ConversionAPI.py
+++ src/backend/ConversionAPI.py
@@ -8,32 +8,40 @@ defaultValue = -999
 
 @app.route('/')
 def convert():
-    IF = request.args.get('F', default = defaultValue, type = float)
-    IC = request.args.get('C', default = defaultValue, type = float)
+    # Get Values from get
+    IF = request.args.get('F', doNoInputError = defaultValue, type = float)
+    IC = request.args.get('C', doNoInputError = defaultValue, type = float)
+
+    #Filler json data
     data = json.jsonify({"return": "Methods Failed to load"})
-    #Error checking and mthod selection
-    if (IF == defaultValue and IC == defaultValue):
-      data = default()
-    elif (IF == defaultValue and IC != defaultValue):
-      data = CtoF(IC)
-    elif (IF != defaultValue and IC == defaultValue):
-      data = FtoC(IF)
-    else:
-      data = error()
+
+    #Error checking and method selection
+    if (IF == defaultValue and IC == defaultValue): # No inputs
+      data = doNoInputError()
+    elif (IF == defaultValue and IC != defaultValue): # F inputed
+      data = covertCtoF(IC)
+    elif (IF != defaultValue and IC == defaultValue): # C inputed
+      data = convertFtoC(IF)
+    else: # Too many inputs
+      data = doTooManyInputError()
     return data
 
-def default():
+# Not enough Input Error
+def doNoInputError():
     return json.jsonify({"return": "No input"})
 
-def FtoC(F1):
+# Farenheit to Celcius
+def convertFtoC(F1):
     C1 = (F1 - 32) * (5 / 9)
     return json.jsonify({"return": C1})
 
-def CtoF(C2):
+# Celcius to Farenheit
+def covertCtoF(C2):
     F2 = (C2 * 9 / 5) + 32
     return json.jsonify({"return": F2})
 
-def error():
+# Too Many Input Error
+def doTooManyInputError():
   return json.jsonify({"return": "Too many input"})
 
 if __name__ == "__main__":
